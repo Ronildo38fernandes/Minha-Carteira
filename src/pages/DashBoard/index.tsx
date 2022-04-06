649c28e725037d02976142550a4b442a82a2ea4d
Ronildo Fernandes <ronildo38fernandes@hotmail.com>
2022-04-06 13:48:42 -0300

    Adicionado select com header dinamico

--- src/pages/DashBoard/index.tsx
+++ src/pages/DashBoard/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import ContentHeader from "../../ContentHeader";
+import ContentHeader from '../../components/ContentHeader';
+import SelectInput from "../../components/SelectInput";
 import { Container } from './style'
 const Dashboard : React.FC = () =>{
-            return(
+           
+   const options = [
+      {value: 'Ronildo',  label: 'Ronildo'},
+      {value: 'Jonas',    label: 'Jonas'},
+      {value: 'Beatriz',  label: 'Beatriz'},
+ ]
+   
+   
+   return(
             
             <Container>
-               <ContentHeader />
+               <ContentHeader title="Dashboard" lineColor="#F7931f">
+                  <SelectInput options={options} />
+               </ContentHeader>
+               
+              
             </Container>
 
             );
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { Container } from "./style";
+import ContentHeader from "../../components/ContentHeader";
+import SelectInput from "../../components/SelectInput";
 
 const List : React.FC = () =>{
+            const options = [
+                        {value: 'Ronildo',  label: 'Ronildo'},
+                        {value: 'Jonas',    label: 'Jonas'},
+                        {value: 'Beatriz',  label: 'Beatriz'},
+                   ]
+
+
             return(
-             <h1>List</h1>
+             <Container>           
+               <ContentHeader title="Saídas" lineColor="#E44C4E">
+                  <SelectInput options={options} />
+               </ContentHeader>
+            
+             </Container>
             );
 }
 export default List;
